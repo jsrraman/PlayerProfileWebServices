15f8e6bd25fc9b26e8d5b893e5547182621098d1
Rajaraman <rajaraman.ks@gmail.com>
2015-11-21 17:28:16 +0530

    Enable CORS (Cross Origin Resource Sharing) access to the server

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// Note: This below response header appending needs to be at the beginning to enable
+// CORS (Cross Origin Resource Sharing for all domains). Otherwise CORS will not be enabled.
+// Basically this intercepts the request and add the required header in the response
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -15,7 +24,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,7 +37,7 @@ app.use('/scrape', scrapeRoute);
 app.use('/players', playersRoute);
 
 // For other undefined paths, catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -39,7 +48,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -50,7 +59,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to users
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
